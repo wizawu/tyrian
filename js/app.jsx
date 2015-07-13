a1dc8046a2a49bd6e170581a6e00ab7e45624cc3
wizawu <wizawu@gmail.com>
2015-07-14 00:26:57 +0800

    app and Home

---
+++ js/app.jsx
@@ -0,0 +1,47 @@
+require("./actions/mockxhr.js");
+
+const {
+    React,
+    ReactRouter,
+    i18n,
+} = global.__;
+
+const {DefaultRoute, Route, RouteHandler} = ReactRouter;
+
+const Home = require("./components/Home.jsx");
+const translation = require("./translation.js");
+
+i18n.init({resStore: translation});
+
+const App = React.createClass({
+    propTypes: {
+        i18n: React.PropTypes.object
+    },
+
+    getInitialState() {
+        return { language: "en-US" };
+    },
+
+    componentWillMount() {
+        this.props.i18n.setLng(this.state.language);
+    },
+
+    componentWillUpdate() {
+        this.props.i18n.setLng(this.state.language);
+    },
+
+    render() {
+        return <div><RouteHandler i18n={this.props.i18n} /></div>;
+    }
+});
+
+let route = (
+    <Route path="/" handler={App}>
+        <Route path="home" handler={Home} />
+        <DefaultRoute handler={Home} />
+    </Route>
+);
+
+ReactRouter.run(route, Handler => {
+    React.render(<Handler i18n={i18n} />, document.body);
+});
---
+++ js/components/Home.jsx
@@ -0,0 +1,55 @@
+const {
+    React,
+} = global.__;
+
+const LoginActions = require("../actions/LoginActions.js");
+const LoginStore = require("../stores/LoginStore.js");
+
+const Home = React.createClass({
+    propTypes: {
+        i18n: React.PropTypes.object
+    },
+
+    getInitialState() {
+        return {
+            email: ""
+        };
+    },
+
+    componentDidMount() {
+        LoginStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount() {
+        LoginStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange() {
+        this.setState({
+            email: (LoginStore.getAll() || {}).email
+        });
+    },
+
+    _signIn() {
+        LoginActions.login({user: this.refs.input.getDOMNode().value});
+    },
+
+    _signOut() {
+        LoginActions.logout();
+    },
+
+    render() {
+        let i18n = this.props.i18n;
+
+        return (
+            <div>
+                <input type="text" ref="input" />
+                <button onClick={this._signIn}>{i18n.t("common.signin")}</button>
+                <button onClick={this._signOut}>{i18n.t("common.signout")}</button>
+                <div>{this.state.email}</div>
+            </div>
+        );
+    }
+});
+
+export default Home;

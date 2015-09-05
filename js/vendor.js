0805f34de752dea8809dfca5b4b6546fedf38cd4
wizawu <wizawu@gmail.com>
2015-09-05 19:12:14 +0800

    Replace i18next with i18next-client

--- js/vendor.js
+++ js/vendor.js
@@ -5,11 +5,10 @@ global.__ = assign(global.__ || {}, {
     EventEmitter: require("events").EventEmitter,
     FluxDispatcher: require("flux").Dispatcher,
     Mockxhr: require("mockxhr"),
-    PolymerLayout: require("react-polymer-layout"),
     ReactRouter: require("react-router"),
     assign: assign,
     clone: x => JSON.parse(JSON.stringify(x)),
-    i18n: require("i18next"),
+    i18n: require("i18next-client"),
     keyMirror: require("react/lib/keyMirror"),
     values: d => Object.keys(d).map(k => d[k])
 });

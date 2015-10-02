5a4ec3e397d6bdf3d8f101e42413bc3febe01afe
wizawu <wizawu@gmail.com>
2015-10-02 19:12:53 +0800

    Move to Redux. #3

--- js/app.jsx
+++ js/app.jsx
@@ -3,45 +3,26 @@ require("./actions/mockxhr");
 const {
     React,
     ReactRouter,
-    i18n,
 } = require("./namespace");
 
-const {DefaultRoute, Route, RouteHandler} = ReactRouter;
+const {IndexRoute, Route, Router} = ReactRouter;
 
 const Home = require("./components/Home");
-const translation = require("./translation");
-
-i18n.init({resStore: translation});
 
 const App = React.createClass({
-    propTypes: {
-        i18n: React.PropTypes.object
-    },
-
-    getInitialState() {
-        return { language: "en-US" };
-    },
-
-    componentWillMount() {
-        this.props.i18n.setLng(this.state.language);
-    },
-
-    componentWillUpdate(_, nextState) {
-        this.props.i18n.setLng(nextState.language);
-    },
-
     render() {
-        return <div><RouteHandler i18n={this.props.i18n} /></div>;
+        return <div>{this.props.children}</div>;
     }
 });
 
-let route = (
-    <Route path="/" handler={App}>
-        <Route path="home" handler={Home} />
-        <DefaultRoute handler={Home} />
-    </Route>
-);
+window.ReactRouter = ReactRouter;
 
-ReactRouter.run(route, Handler => {
-    React.render(<Handler i18n={i18n} />, document.body);
-});
+React.render((
+    <Router>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="home" component={Home} />
+            <Route path="*" component={Home} />
+        </Route>
+    </Router>
+), document.body);

1d9da65138dd2005e5abb7f3808dcab14c51f7a1
wizawu <wizawu@gmail.com>
2017-01-18 00:21:32 +0800

    Add react lib to example

--- example/js/components/App.tsx
+++ example/js/components/App.tsx
@@ -1,7 +1,13 @@
 import Component from "inferno-component"
+import {Box} from "react-polymer-layout"
 
 export default class App extends Component<{}, {}> {
     render() {
-        return <div>Hello, Inferno</div>
+        return (
+            <Box justified>
+                <div>Hello, Inferno</div>
+                <div>{Date.now()}</div>
+            </Box>
+        )
     }
 }

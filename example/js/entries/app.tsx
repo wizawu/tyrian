db9fc9dc0bd988997e3ea89b25f31d79b544cd8c
wizawu <wizawu@gmail.com>
2017-01-18 23:20:58 +0800

    declare React by default

--- example/js/entries/app.tsx
+++ example/js/entries/app.tsx
@@ -3,4 +3,6 @@ import "../../css/app.less"
 import Inferno from "inferno"
 import App from "../components/App"
 
+console.log(window.InfernoBeaker)
+
 Inferno.render(<App />, document.querySelector("section"))

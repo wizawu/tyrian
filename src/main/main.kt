ecc463737e3d3828ae7fa4ee11d2c1c5885f7043
wizawu <wizawu@gmail.com>
2017-01-27 17:17:15 +0800

    Add experimental files

---
+++ src/main/main.kt
@@ -0,0 +1,5 @@
+package main
+
+fun main(args: Array<String>) {
+    println("You should run `gradle test`")
+}
---
+++ src/test/HelloTest.kt
@@ -0,0 +1,11 @@
+package orsql.test
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+class HelloTest() {
+    @Test
+    fun testAssert() {
+        assertTrue(1 == 3 - 2)
+    }
+}

4510cfd851e0ceff057b6143a77e114e6808f79f
wizawu <wizawu@gmail.com>
2017-01-30 21:18:53 +0800

    Change tcpKeepAlive to autoReconnect

--- src/orsql/connection.kt
+++ src/orsql/connection.kt
@@ -35,7 +35,7 @@ open class MySQLConnection(options: ConnectOptions) : IConnection {
     init {
         if (this.javaClass.name == "orsql.MySQLConnection") {
             val url = String.format(
-                    "jdbc:mysql://%s:%d/%s?user=%s&password=%s&tcpKeepAlive=true",
+                    "jdbc:mysql://%s:%d/%s?user=%s&password=%s&autoReconnect=true",
                     options.server, options.port, options.database, options.user, options.password
             )
             connection = DriverManager.getConnection(url)
@@ -134,7 +134,7 @@ open class MySQLConnection(options: ConnectOptions) : IConnection {
 class MariaDBConnection(options: ConnectOptions) : MySQLConnection(options) {
     init {
         val url = String.format(
-                "jdbc:mariadb://%s:%d/%s?user=%s&password=%s&tcpKeepAlive=true",
+                "jdbc:mariadb://%s:%d/%s?user=%s&password=%s&autoReconnect=true",
                 options.server, options.port, options.database, options.user, options.password
         )
         connection = DriverManager.getConnection(url)

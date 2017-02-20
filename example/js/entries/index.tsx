9779158cab5450f858e8decc3bc5881c8424df44
wizawu <wizawu@gmail.com>
2017-02-20 23:48:51 +0800

    Fix react-router with hashHistory

--- example/js/entries/index.tsx
+++ example/js/entries/index.tsx
@@ -2,14 +2,14 @@ import "../../css/app.less"
 
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import {Router, Route, IndexRedirect, browserHistory} from "react-router"
+import {Router, Route, IndexRedirect, hashHistory} from "react-router"
 
 import App from "../components/App"
 
 console.log(window.React.version)
 
 ReactDOM.render((
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={App}>
             <IndexRedirect to="app" />
             <Route path="app" component={null} />
--- react-lib.js
+++ react-lib.js
@@ -1,2 +1,8 @@
 window.React = require("react")
 window.ReactDOM = require("react-dom")
+window.ReactRouter = {
+    IndexRedirect: require("react-router/lib/IndexRedirect"),
+    Route: require("react-router/lib/Route"),
+    Router: require("react-router/lib/Router"),
+    hashHistory: require("react-router/lib/hashHistory"),
+}

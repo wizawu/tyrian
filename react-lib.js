ba50c7088a3394ce12129dbc59222825d91d40a2
wizawu <wizawu@gmail.com>
2017-03-16 02:55:16 +0800

    Export everything in react-router

--- react-lib.js
+++ react-lib.js
@@ -1,8 +1,3 @@
 window.React = require("react")
 window.ReactDOM = require("react-dom")
-window.ReactRouter = {
-    IndexRedirect: require("react-router/lib/IndexRedirect"),
-    Route: require("react-router/lib/Route"),
-    Router: require("react-router/lib/Router"),
-    hashHistory: require("react-router/lib/hashHistory"),
-}
+window.ReactRouter = require("react-router")

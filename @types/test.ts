767b10b637082e5574a571ac3fdd1f6323032bc3
wizawu <wizawu@gmail.com>
2017-03-31 02:41:13 +0800

    Update @types/jdk

--- @types/test.ts
+++ @types/test.ts
@@ -1,3 +1,5 @@
-/// <reference path="jdk/rt.d.ts" />
+// <reference path="jdk/jconsole.d.ts" />
+// <reference path="jdk/rt.d.ts" />
+/// <reference path="jdk/tools.d.ts" />
 
-console.log(java)
+console.log(com);

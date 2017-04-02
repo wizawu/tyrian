3934e7a166cbc7a85addd75c908f7304183094bd
wizawu <wizawu@gmail.com>
2017-04-03 00:56:08 +0800

    Reformat

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,10 +1,10 @@
 import * as fs from "fs"
 import * as path from "path"
-import {spawnSync} from "child_process"
+import { spawnSync } from "child_process"
 
-export default function(target: string) {
+export default function (target: string) {
     let lib = path.resolve(path.dirname(target) + "/../lib")
     let classpath = fs.readdirSync(lib).map(jar => jar === "@types" ? "" : `${lib}/${jar}`).join(":")
-    let child = spawnSync("jjs", ["-cp", classpath, target], {stdio: "inherit"})
+    let child = spawnSync("jjs", ["-cp", classpath, target], { stdio: "inherit" })
     process.exit(child.status)
 }

42683c1f789b5752d608911a52d6117141a55bed
wizawu <wizawu@gmail.com>
2017-04-11 03:31:43 +0800

    Add @types/nashorn.d.ts

---
+++ @types/nashorn.d.ts
@@ -0,0 +1,6 @@
+declare class Java {
+    static extend(type: any, impl: any): any
+    static from(value: any): any
+    static to(value: any, type: any): any
+    static type(className: string): any
+}

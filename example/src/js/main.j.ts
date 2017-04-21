abc7049a560a4faa119df2477ea772691e4b6232
wizawu <wizawu@gmail.com>
2017-04-21 22:26:41 +0800

    Update example

--- example/src/js/main.j.ts
+++ example/src/js/main.j.ts
@@ -1,1 +1,14 @@
-org.pmw.tinylog.Logger.info(java.lang.System.getProperty("java.version"))
+import { constructor as Chalk } from "chalk"
+
+const chalk = new Chalk({ enabled: true })
+
+class ColorfulJSON {
+    date: string = chalk.yellow(new Date().toJSON())
+    random: string = chalk.green(Math.random().toString())
+}
+
+;(print as any)(JSON.stringify(new ColorfulJSON()))
+
+;(print as any)(Packages.ro)
+let ro = Packages.ro
+;(print as any)(ro.pippo.core.Pippo)

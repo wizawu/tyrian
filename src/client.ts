795618a8766bfe2b76b102adf54ae6252163962e
wizawu <wizawu@gmail.com>
2017-05-30 20:36:38 +0800

    Define interface Client

--- src/client.ts
+++ src/client.ts
@@ -10,17 +10,44 @@ export interface Options {
     useSSL?: boolean
 }
 
-interface ConnectionImpl {
-    ensureTable(tableName: string)
-    ensureColumn(tableName: string, columnName: string, columnType: string)
-    ensurePrimaryKey(tableName: string, columnName: string)
-    ensureIndex(tableName: string, columnNames: string[])
-    ensureUniqueIndex(tableName: string, columnNames: string[])
-    one<T>(sql: string, parameters: any[]): T | null
-    list<T>(sql: string, parameters: any[]): T[]
-    save(tableName: string, obj: any, primary: string)
-    execute(sql: string, parameters: any[])
+export interface Client {
+    // Key-value store
+    getInt(key: string): number | null
+    getInt(bucket: string, key: string): number | null
+    getFloat(key: string): number | null
+    getFloat(bucket: string, key: string): number | null
+    getString(key: string): string | null
+    getString(bucket: string, key: string): string | null
+    getJSON(key: string): any | null
+    getJSON(bucket: string, key: string): any | null
+    setInt(key: string, value: number, ttl?: number)
+    setInt(bucket: string, key: string, value: number, ttl?: number)
+    setFloat(key: string, value: number, ttl?: number)
+    setFloat(bucket: string, key: string, value: number, ttl?: number)
+    setString(key: string, value: string, ttl?: number)
+    setString(bucket: string, key: string, value: string, ttl?: number)
+    setJSON(key: string, json: any, ttl?: number)
+    setJSON(bucket: string, key: string, json: any, ttl?: number)
+
+    // RDBMS
+    ensureTable(table: string, pkey: string, type: string)
+    ensureColumn(table: string, column: string, type: string)
+    ensureIndex(table: string, columns: string[])
+    ensureUniqueIndex(table: string, columns: string[])
+    one<T>(sql: string, parameters?: any[]): T | null
+    list<T>(sql: string, parameters?: any[]): T[]
+    insert(table: string, object: any)
+    update(table: string, object: any)
+    save(table: string, object: any)
+    execute(sql: string, parameters?: any[])
+
+    // Object store
+    get(key: string): java.lang.Byte[]
+    get(bucket: string, key: string): java.lang.Byte[]
+    put(key: string, data: java.lang.Byte[], ttl?: number)
+    put(bucket: string, key: string, data: java.lang.Byte[], ttl?: number)
+
+    delete(table: string, key: any)
+    delete(bucket: string, key: string)
     close()
 }
-
-export default ConnectionImpl

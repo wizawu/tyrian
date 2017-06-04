5b97b5c26d57c0f97b7e0fc772088f946add61dc
wizawu <wizawu@gmail.com>
2017-06-04 16:59:01 +0800

    Remove example

--- example/src/js/main.j.ts
+++
@@ -1,6 +0,0 @@
-import { constructor as Chalk } from "chalk"
-
-const chalk = new Chalk({ enabled: true })
-const Spark = spark.Spark
-
-Spark.get("/hello", (req, res) => chalk.green("Hello World"))

61756c52eedaf0496dfd914cda9ae92d2b64434f
wizawu <wizawu@gmail.com>
2017-06-05 17:28:40 +0800

    Update help message

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -12,10 +12,10 @@ Version: ${version(instdir)}
 Usage:
   1c help                               print this message
   1c version                            print the version number
-  1c env                                check if all dependencies of 1c are ready
+  1c env                                check all prerequisites of 1c
   1c install                            install dependencies in package.json
   1c build [options] <entry>...         build one/multiple entries (.ts/.tsx)
-  1c run [-w] [jjs options] <output>    run output file (.js)
+  1c run [-w] [jjs options] <output>    run an output file (.js)
 
 build options:
   -c <dir>          output build result in <dir>

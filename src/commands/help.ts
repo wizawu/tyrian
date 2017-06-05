52c72e2b0eca687432927368895fafcec7a1e862
wizawu <wizawu@gmail.com>
2017-06-05 14:57:08 +0800

    Update help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -14,14 +14,13 @@ Usage:
   1c version                                        print the version number
   1c env                                            check if all dependencies of 1c are ready
   1c install                                        install dependencies in package.json
-  1c build [-w] [-m] [-c dir | -o file] <entry>     build one entry (.ts/.tsx)
-  1c build [-w] [-c dir] <entry1> <entry2> ...      build multiple entries (.ts/.tsx)
+  1c build [-w] [-m] [-c dir|-o file] <entry>...    build one/multiple entries (.ts/.tsx)
   1c run [-w] [jjs options] <output>                run output file (.js)
 
 Options:
 
   -c <dir>          output build result in <dir>
-  -m                build the entry as a commonjs module
+  -m                build each entry as a commonjs module
   -o <file>         output build result to <file>
   -w                watch changes and re-build/re-run
   jjs options       check out 'jjs -h'

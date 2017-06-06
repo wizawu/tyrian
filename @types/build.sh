f726fc14d8a87beb6a1e127ee019addb38f54b68
wizawu <wizawu@gmail.com>
2017-06-07 00:23:18 +0800

    Remove @types/test.ts

--- @types/build.sh
+++ @types/build.sh
@@ -20,5 +20,10 @@ ls *.d.ts | grep -v index.d.ts | xargs -I {} echo "/// <reference path=\"{}\" />
 
 # run test
 
-cd $dir
-tsc --noEmit --skipLibCheck test.ts
+test=/tmp/1c@types_test.ts
+
+echo "/// <reference path='$dir/nashorn.d.ts' />" > $test
+echo "/// <reference path='$dir/jdk/index.d.ts' />" >> $test
+grep -h "declare namespace" *.d.ts | sort | uniq | awk '{print $3}' | xargs -I {} echo "Boolean({})" >> $test
+
+tsc --noEmit --skipLibCheck $test
--- @types/test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="nashorn.d.ts" />
-/// <reference path="jdk/index.d.ts" />
-
-console.log(com)
-console.log(java)
-console.log(javax)
-console.log(jdk)
-console.log(org)
-console.log(sun)

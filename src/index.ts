69ced33699230e6597801204b49fa32fcbe14e5f
wizawu <wizawu@gmail.com>
2018-04-05 21:52:40 +0800

    Merge tooq

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import "core-js/es6/set"
 import "core-js/es6/symbol"
 
 import * as _logging from "./logging"
+import * as _mysql from "./mysql"
 
 export const logging = _logging
+export const mysql = _mysql

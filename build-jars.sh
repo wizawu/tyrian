a8351638143eb65291c687f1236a52f8efea314a
wizawu <wizawu@gmail.com>
2018-07-18 14:53:50 +0800

    Fix build-jars

--- build-jars.sh
+++ build-jars.sh
@@ -2,8 +2,14 @@
 
 mkdir -p jars
 cd jars
-git clone --depth 1 https://github.com/wizawu/openjdk.git
+
+wget -nc -O openjdk.tgz http://hg.openjdk.java.net/jdk/jdk/archive/tip.tar.bz2
+root=$(tar tf openjdk.tgz | head -1 | sed -e 's/\/.*//')
+tar xf openjdk.tgz --wildcards $root/src/java.* $root/src/jdk.*
+
+mv $root openjdk
 cd openjdk
+
 find src/ -type d -name 'classes' \
     | grep 'share/classes' \
     | awk -F'/' '{print $2}' \

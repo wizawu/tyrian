75252cb144466d7d19f789edc9db1f53836741df
wizawu <wizawu@gmail.com>
2018-09-16 12:54:26 +0800

    Fix crypto.getRandomValues

--- std/dist/node-polyfill.js
+++ std/dist/node-polyfill.js
@@ -5,7 +5,28 @@ _.crypto = {
     getRandomValues: function (array) {
         var result = [];
         for (var i = 0; i < array.length; i++) {
-            result.push(random.nextInt(256));
+            switch (array.constructor.name) {
+                case "Int8Array":
+                    result.push(random.nextInt(256) - 128);
+                    break;
+                case "Uint8Array":
+                    result.push(random.nextInt(256));
+                    break;
+                case "Int16Array":
+                    result.push(random.nextInt(Math.pow(2, 16)) - Math.pow(2, 15));
+                    break;
+                case "Uint16Array":
+                    result.push(random.nextInt(Math.pow(2, 16)));
+                    break;
+                case "Int32Array":
+                    result.push(random.nextLong() % Math.pow(2, 32) - Math.pow(2, 31));
+                    break;
+                case "Uint32Array":
+                    result.push(random.nextLong() % Math.pow(2, 32));
+                    break;
+                default:
+                    result.push(random.nextInt(256));
+            }
         }
         return result;
     }
--- std/src/node-polyfill.ts
+++ std/src/node-polyfill.ts
@@ -4,10 +4,31 @@ const _ = global as any
 _.self = global
 
 _.crypto = {
-    getRandomValues(array: Uint8Array) {
+    getRandomValues(array: Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array) {
         let result: number[] = []
         for (let i = 0; i < array.length; i++) {
-            result.push(random.nextInt(256))
+            switch (array.constructor.name) {
+                case "Int8Array":
+                    result.push(random.nextInt(256) - 128)
+                    break
+                case "Uint8Array":
+                    result.push(random.nextInt(256))
+                    break
+                case "Int16Array":
+                    result.push(random.nextInt(Math.pow(2, 16)) - Math.pow(2, 15))
+                    break
+                case "Uint16Array":
+                    result.push(random.nextInt(Math.pow(2, 16)))
+                    break
+                case "Int32Array":
+                    result.push(random.nextLong() % Math.pow(2, 32) - Math.pow(2, 31))
+                    break
+                case "Uint32Array":
+                    result.push(random.nextLong() % Math.pow(2, 32))
+                    break
+                default:
+                    result.push(random.nextInt(256))
+            }
         }
         return result
     }

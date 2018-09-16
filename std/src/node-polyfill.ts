96f7e3dd6e6aa4670bd2247501814b3ace9c33de
wizawu <wizawu@gmail.com>
2018-09-17 02:42:29 +0800

    Revert "Add innerHeight/innerWidth/window polyfill"
    This reverts commit 53c2c26008a2c3e0339eb1c9b998604dcfe7d82c.

--- std/src/node-polyfill.ts
+++ std/src/node-polyfill.ts
@@ -1,10 +1,7 @@
 const random = new java.security.SecureRandom()
 const _ = global as any
 
-_.innerHeight = 0
-_.innerWidth = 0
 _.self = global
-_.window = global
 
 _.setTimeout = (runnable: java.lang.Runnable, delay = 0) => {
     let thread = new java.lang.Thread(() => {

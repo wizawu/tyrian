7a55ef2607d670b2c966651eefa6d2fe6db1b24c
wizawu <wizawu@gmail.com>
2018-10-10 04:35:40 +0800

    Put jcenter behind mavenCentral

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -44,8 +44,8 @@ const build_gradle = deps => `
     apply plugin: "java"
 
     repositories {
-        jcenter()
         mavenCentral()
+        jcenter()
     }
 
     task install(type: Copy) {

943dc148e22284ba32ce29cc61f5c11daca21ab0
wizawu <wizawu@gmail.com>
2023-02-13 00:38:17 +0800

    Publish 11.1.3

---
+++ rollup.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  external: [
+    "@babel/preset-env",
+    "@rollup/plugin-babel",
+    "@rollup/plugin-commonjs",
+    "@rollup/plugin-json",
+    "@rollup/plugin-node-resolve",
+    "@rollup/plugin-typescript",
+    "rollup",
+  ],
+  output: {
+    format: "cjs",
+    generatedCode: "es5",
+  },
+}
